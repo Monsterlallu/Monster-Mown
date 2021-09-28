7ae27b0d071e8d3480f5a76a67761b682ab762a7
Monsterlallu <87799191+Monsterlallu@users.noreply.github.com>
2021-09-28 13:48:18 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,44 +12,7 @@ const path = require("path");
 const events = require("./events");
 const chalk = require('chalk');
 const config = require('./config');
-const axios = require('axios');
-const Heroku = require('heroku-client');
-const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
-const {Message, StringSession, Image, Video} = require('./whatsasena/');
-const { DataTypes } = require('sequelize');
-const { GreetingsDB, getMessage } = require("./plugins/sql/greetings");
-const got = require('got');
-const simpleGit = require('simple-git');
-const git = simpleGit();
-const crypto = require('crypto');
-const nw = '```Blacklist Defected!```'
-const heroku = new Heroku({
-    token: config.HEROKU.API_KEY
-});
-let baseURI = '/apps/' + config.HEROKU.APP_NAME;
-const Language = require('./language');
-const Lang = Language.getString('updater');
-
-// Sql
-const WhatsAsenaDB = config.DATABASE.define('WhatsAsenaDuplicated', {
-    info: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    value: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    }
-});
-fs.readdirSync('./plugins/sql/').forEach(plugin => {
-    if(path.extname(plugin).toLowerCase() == '.js') {
-        require('./plugins/sql/' + plugin);
-    }
-});
-const plugindb = require('./plugins/sql/plugin');
-var OWN = { ff: '905511384572,0' }
-// Yalnızca bir kolaylık. https://stackoverflow.com/questions/4974238/javascript-equivalent-of-pythons-format-function //
-String.prototype.format = function () {
+const axios = 
     var i = 0, args = arguments;
     return this.replace(/{}/g, function () {
       return typeof args[i] != 'undefined' ? args[i++] : '';
